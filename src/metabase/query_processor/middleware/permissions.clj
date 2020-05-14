6ccb4d736ffccbba288044415bdc49b627f4effe
Alexandru Nagy <alexandru.nagy@revolut.com>
2020-05-14 15:40:42 +0100

    Use AND when checking query permissions

--- src/metabase/query_processor/middleware/permissions.clj
+++ src/metabase/query_processor/middleware/permissions.clj
@@ -50,8 +50,8 @@
   (when *current-user-id*
     (log/tracef "Checking query permissions. Current user perms set = %s" (pr-str @*current-user-permissions-set*))
     (if card-id
-      (check-card-read-perms card-id)
-      (check-ad-hoc-query-perms outer-query))))
+      (and [(check-card-read-perms card-id)
+      (check-ad-hoc-query-perms outer-query)])))
 
 (defn check-query-permissions
   "Middleware that check that the current user has permissions to run the current query. This only applies if
