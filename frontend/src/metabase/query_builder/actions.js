b1b7c5e15586659abfb7e1e9adf3c2c9c60f6996
canhduong28 <canh.duong@revolut.com>
2020-08-13 17:54:48 +0100

    DE-4784 Disable api/database?include=tables endpoint

--- frontend/src/metabase/query_builder/actions.js
+++ frontend/src/metabase/query_builder/actions.js
@@ -301,7 +301,7 @@ export const initializeQB = (location, params) => {
     dispatch(cancelQuery());
 
     // preload metadata that's used in DataSelector
-    dispatch(Databases.actions.fetchList({ include: "tables" }));
+    // dispatch(Databases.actions.fetchList({ include: "tables" }));
     dispatch(Databases.actions.fetchList({ saved: true }));
 
     const { currentUser } = getState();
